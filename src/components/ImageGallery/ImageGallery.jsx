05f24a7fa4c3871cae98b7584f5dfe4ad4ccb972
EnVy <envy7321@gmail.com>
2022-10-07 17:35:30 +0300

    save

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -5,28 +5,25 @@ import fetchRequest from 'components/services/FetchApi';
 import Modal from 'components/shared/Modal/Modal';
 import { Div, LoadMode } from './imageGallery.styled';
 import React from 'react';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function ImageGallery({ searchImages }) {
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [page, setPage] = useState(1);
-  const [modalOpen, setModalOpen] = useState('');
+  const [modalOpen, setModalOpen] = useState(false);
 
   const [urlLarge, setUlrLarge] = useState('');
   const [title, setTitle] = useState('');
 
-  const prevPage = Prev(page);
-  const prevSearchImages = Prev(searchImages);
-
-  const loadMore = () => {
-    setPage(prevPage => prevPage + 1);
-  };
   useEffect(() => {
     const fetchImages = async (currentName, currentPage) => {
-      setLoading(true);
+      if (!searchImages) {
+        return;
+      }
       try {
+        setLoading(true);
         const result = await fetchRequest(currentName, currentPage);
         const items = result.hits;
         if (items.length === 0) {
@@ -34,30 +31,17 @@ export default function ImageGallery({ searchImages }) {
             "We didn't find your request, please try again later"
           );
         }
-        if (currentPage === 1) {
-          setImages([...items]);
-        } else {
-          setImages(prev => [...prev, ...items]);
-        }
+
+        setImages(prev => [...prev, ...items]);
       } catch (error) {
         setError(error);
       } finally {
         setLoading(false);
       }
     };
-    if (!searchImages) {
-      return;
-    }
-    if (page > prevPage) {
-      fetchImages(searchImages, page);
-      return;
-    }
-    if (prevSearchImages !== searchImages && page === prevPage) {
-      fetchImages(searchImages, 1);
-      setPage(1);
-      return;
-    }
-  }, [searchImages, page, prevPage, prevSearchImages]);
+    fetchImages();
+  }, [searchImages, page]);
+
   const openModal = (urlLarge, title) => {
     setModalOpen(true);
     setUlrLarge(urlLarge);
@@ -68,13 +52,10 @@ export default function ImageGallery({ searchImages }) {
     setUlrLarge('');
     setTitle('');
   };
-  function Prev(value) {
-    const prev = useRef();
-    useEffect(() => {
-      prev.current = value;
-    });
-    return prev.current;
-  }
+
+  const loadMore = () => {
+    setPage(state => state + 1);
+  };
   const isImages = Boolean(images.length);
   return (
     <div>
